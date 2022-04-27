d8f62a644c7da2ab9a3b75a66b12af5444e53190
Jhonny Castro Clavijo <johnnycastro72@msn.com>
2022-04-27 15:02:20 -0500

    Controller
    Game Controller class created

--- src/main/java/com/crud/crudbook/controller/GameController.java
+++ src/main/java/com/crud/crudbook/controller/GameController.java
@@ -1,9 +1,11 @@
 package com.crud.crudbook.controller;
 
+import com.crud.crudbook.entity.Game;
 import com.crud.crudbook.service.GameServiceInterface;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/v1/")
@@ -12,5 +14,23 @@ public class GameController {
     @Autowired
     private GameServiceInterface service;
 
+    @GetMapping("get/games")
+    public List<Game> getAllGames() {
+        return service.getAllGames();
+    }
 
+    @PostMapping("save/game")
+    public Game saveGame(@RequestBody Game game) {
+        return service.saveGame(game);
+    }
+
+    @PostMapping("update/game")
+    public Game updateGame(@RequestBody Game game) {
+        return service.updateGame(game);
+    }
+
+    @DeleteMapping("delete/game/{id}")
+    public void deleteGame(@PathVariable Long id) {
+        service.deleteGame(id);
+    }
 }
